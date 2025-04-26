8f2bcc65a01f37b50c394f6f5e1a800e950e9c0f
Piyushrathoree <meonpath008@gmail.com>
2025-04-27 00:36:23 +0530

    feat: initialize project with TypeScript, Express, and MongoDB setup
    - Added package.json with dependencies and scripts for building and running the server.
    - Created TypeScript configuration (tsconfig.json) for the project.
    - Implemented database connection logic in src/db/db.ts using Mongoose.
    - Set up basic Express server in src/index.ts with a sample route.
    - Added placeholder files for user controller, model, middleware, and routes.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+
+
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_, res) => {
+    res.send('Hello World! im here');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}) 
