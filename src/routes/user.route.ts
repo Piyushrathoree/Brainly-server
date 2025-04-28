33943af91972d7f6d6a44e966836674247e7e80b
Piyushrathoree <meonpath008@gmail.com>
2025-04-29 01:39:34 +0530

    feat: expand user authentication routes to include verification, password reset, and logout functionality

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { LoginUser, RegisterUser } from "../controllers/user.controller";
+import { ForgotPassword, LoginUser, LogoutUser, RegisterUser, ResetPassword, VerifyUser } from "../controllers/user.controller";
+import authMiddleware from "../middlewares/auth.middleware";
 
 
 const AuthRouter = Router();
 
 AuthRouter.post("/register", RegisterUser)
 AuthRouter.post("/login", LoginUser)
+AuthRouter.post("/verify", VerifyUser);
+AuthRouter.post("/forgot-password", ForgotPassword);
+AuthRouter.post("/reset-password/:token", ResetPassword);
+AuthRouter.post('/logout',authMiddleware, LogoutUser)
 
 export default AuthRouter;
