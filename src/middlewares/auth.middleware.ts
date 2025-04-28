9f6dcacebf3e54fbb4c0546c721c03065db0e211
Piyushrathoree <meonpath008@gmail.com>
2025-04-29 01:39:39 +0530

    feat: implement authentication middleware for token verification and user extraction

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,54 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+
+const authMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void => {
+    const token = req.cookies?.token;
+    console.log(token);
+    
+    if (!token) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
+    try {
+        if (!process.env.JWT_SECRET) {
+            throw new Error(
+                "JWT_SECRET is not defined in environment variables"
+            );
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded) {
+            res.status(401).json({ message: "Invalid token" });
+            return;
+        }
+        if (typeof decoded === "object" && decoded !== null) {
+            req.user = decoded as {
+                _id: string;
+                name: string;
+                email: string;
+                isVerified: boolean;
+                lastLogin?: Date;
+                verificationCode?: string;
+                verificationCodeExpires?: Date;
+                resetPasswordToken?: string;
+                resetPasswordTokenExpires?: Date;
+            };
+        } else {
+            throw new Error("Invalid token payload");
+        }
+        if (!req.user) {
+            res.status(401).json({ message: "Unauthorized" });
+            return;
+        }
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token" });
+        return;
+    }
+};
+
+export default authMiddleware;
