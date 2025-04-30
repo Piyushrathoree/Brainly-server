d4804cdfb4f92158e5f31221645d0dc666278e30
Piyushrathoree <meonpath008@gmail.com>
2025-05-01 05:24:09 +0530

    feat: add generateHash utility function and update generateVerificationCode, include uuid dependency

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,11 @@
 export const generateVerificationCode = (): string => {
-
-    const code = Math.floor(100000 + Math.random()* 900000).toString();
-    console.log(code);
-    
+    const code = Math.floor(100000 + Math.random() * 900000).toString();
     return code;
 }
 
+import { v4 as uuidv4 } from 'uuid';
+
+export const generateHash = (): string => {
+    const hash = uuidv4().replace(/-/g, ''); // Remove dashes from UUID
+    return hash.substring(0, 16); // Return the first 16 characters
+}
