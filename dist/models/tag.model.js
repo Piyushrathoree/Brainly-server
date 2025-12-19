f9efd49084ec4838a52d6f06e65282463bf6806b
Piyushrathoree <meonpath008@gmail.com>
2025-12-19 17:43:59 +0530

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- dist/models/tag.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const tagSchema = new mongoose_1.Schema({
-    title: { type: String, required: true, unique: true, trim: true },
-}, { timestamps: true });
-const Tag = (0, mongoose_1.model)("Tag", tagSchema);
-exports.default = Tag;
--- dist/routes/tag.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const TagRouter = (0, express_1.Router)();
-const tags_controller_1 = require("../controllers/tags.controller");
-TagRouter.post("/create", tags_controller_1.createTag);
-TagRouter.get("/All", tags_controller_1.showAllTag);
-TagRouter.get("/content", tags_controller_1.getContentByTag);
-exports.default = TagRouter;
